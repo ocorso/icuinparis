5dc93b26e9f826770c0ff1210261b94068899fde
Owen Corso <owen@click3x.com>
2013-07-17 18:35:06 -0400

    EOD Commit

--- store/js/icu_store.js
+++ store/js/icu_store.js
@@ -6,17 +6,55 @@ var google_conversion_color 	= "666666";
 var google_conversion_label 	= "FVa8CP-p6AIQ6bnl4AM";
 var google_conversion_value 	= 0;
 
-function showBigImageDiv(packName, packId) {
-	alert(packId);
-	document.getElementById(packId).src = packName;
 
+var scroller              = null;
+var showUIDropdownClass   = "b-style-dropdown_state_open";
+var hideUIDropdownClass   = "b-style-dropdown_state_close";
+var UIDropdown            = jQuery(".b-style-dropdown");
+var Translator            = new Translate({"Credit card number doesn't match credit card type":"Credit card number does not match credit card type","Please use only letters (a-z or A-Z), numbers (0-9) or underscore(_) in this field, first character should be a letter.":"Please use only letters (a-z or A-Z), numbers (0-9) or underscores (_) in this field, first character must be a letter."});
+var productAddToCartForm  = new VarienForm('product_addtocart_form');
+productAddToCartForm.submit = function(){
+        if (this.validator.validate()) {
+            this.form.submit();
+        }
+    }.bind(productAddToCartForm);
+
+if(typeof tinyMCE != 'undefined'){
+  tinyMCE.init({
+    // General options
+    mode : "textareas",
+    theme : "simple",
+  });
+  
 }
 
+
 jQuery(document).ready(function($) {
-  $('a[rel*=facebox]').facebox();
   initMenu();
 
-})
+  var options = {
+      zoomWidth: 400,
+      zoomHeight: 400,
+      title: false,
+      xOffset: 0,
+      yOffset: 0,
+      position: "right",
+      zoomType: 'standard',
+      lens:true,
+      preloadImages: false,
+      alwaysOn:false
+  };
+  $(".jqzoom").jqzoom(options);
+  
+  $('a[rel*=facebox]').facebox();
+
+  $('.b-style-dropdown__button').bind('click', trigger);
+});
+
+function showBigImageDiv(largeName, smallName) {
+  document.getElementById('imagebigg').src = smallName;
+  document.getElementById('axss').href = largeName;
+}
 
 function MM_swapImgRestore() { //v3.0
   var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
@@ -41,7 +79,37 @@ function MM_swapImage() { //v3.0
   var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
    if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
 }
+
+
+
+    
+
+//Switcher Dropdown functions
+function trigger() {
+    isDropDownOpen() ? closeUIDropdown() : openUIDropdown();
+    return false;
+}
+
+function openUIDropdown() {
+    jQuery('body').bind('click', closeUIDropdown)
+    UIDropdown.removeClass(hideUIDropdownClass)
+    UIDropdown.addClass(showUIDropdownClass)
+}
+
+function closeUIDropdown() {
+    jQuery('body').unbind('click')
+    UIDropdown.addClass(hideUIDropdownClass)
+    UIDropdown.removeClass(showUIDropdownClass)
+}
+
+function isDropDownOpen() {
+    return UIDropdown.hasClass(showUIDropdownClass) || false;
+}
+
+
+
 function initMenu() {
+
   jQuery('#menu ul').hide();
   jQuery('#menu li a').click(
     function() {	
@@ -85,3 +153,30 @@ function swaparrow(e){
 	}
 }
 
+function showdescriptionpanel(){
+  document.getElementById('descriptionpanel').style.display = "block";
+  document.getElementById('theatelierpanel').style.display = "none";
+  document.getElementById('shippingpanel').style.display = "none";
+  document.getElementById('inquiriespanel').style.display = "none";
+}
+
+function showtheatelierpanel(){
+  document.getElementById('descriptionpanel').style.display = "none";
+  document.getElementById('theatelierpanel').style.display = "block";
+  document.getElementById('shippingpanel').style.display = "none";
+  document.getElementById('inquiriespanel').style.display = "none";
+}
+
+function showshippingpanel(){
+  document.getElementById('descriptionpanel').style.display = "none";
+  document.getElementById('theatelierpanel').style.display = "none";
+  document.getElementById('shippingpanel').style.display = "block";
+  document.getElementById('inquiriespanel').style.display = "none";
+}
+
+function showinquiriespanel(){
+  document.getElementById('descriptionpanel').style.display = "none";
+  document.getElementById('theatelierpanel').style.display = "none";
+  document.getElementById('shippingpanel').style.display = "none";
+  document.getElementById('inquiriespanel').style.display = "block";
+}
